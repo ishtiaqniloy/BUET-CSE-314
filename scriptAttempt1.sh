afbb61beaeee4e9f79a0f2614c9b96278efabc74
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2018-11-11 21:11:34 +0600

    Without missing rollnum in zipname done

--- scriptAttempt1.sh
+++ scriptAttempt1.sh
@@ -72,27 +72,49 @@ do
 		rm "$zipName"
 
 		dirName=$(ls)
+		dirCount=$(ls | wc -l)
 
-		if [ $dirName = $rollnum ]; then
-			echo Correct submission by $rollnum
+		#echo $dirCount
+
+		if  [ $dirCount=1 ] && [ $dirName = $rollnum ]; then 
+			echo Correct submission by $rollnum #done
 			cp "$dirName" -rt ../Output
-			echo $rollnum 10 >> ../Output/Marks.txt
-		elif [ $dirname =~ $rollnum ]; then
-			echo Partially correct submission by $rollnum
-			cp "$dirName" -rt ../Output
-			echo $rollnum 5 >> ../Output/Marks.txt
+			echo $rollnum 10 Correct submission >> ../Output/Marks.txt
+		elif [ $dirCount -gt 1 ]; then
+			echo multiple directory submission by $rollnum #done
+ 			#cp "$dirName" -rt ../Output
+			
+			mkdir ../Output/"$rollnum"
+			ls | while read dNames 
+			do
+				cp "$dNames" -rt ../Output/"$rollnum"
+
+			done
+
+
+			echo $rollnum 0 Multiple directory submission >> ../Output/Marks.txt
+
+		elif [[ $dirName =~ .*$rollnum.* ]]; then
+			echo Partially correct submission by $rollnum #done
+
+			mv "$dirName" "$rollnum"
+			cp "$rollnum" -rt ../Output
+			echo $rollnum 5  Partially correct submission >> ../Output/Marks.txt
 
 		else
 			echo Wrong submission by $rollnum
-			cp "$dirName" -rt ../Output/Extra
-			echo $rollnum 0 >> ../Output/Marks.txt
+			mv "$dirName" "$rollnum"
+			cp "$rollnum" -rt ../Output
+			echo $rollnum 0  Wrong submission. Extracted roll from zipName >> ../Output/Marks.txt
 
 		fi
 
 		cd ..
 
 		rm -rf temp
-
+		rm -f "$zipName"
+	else	#zip name without the roll
+		echo rollnum not found in the zip name #NO TEST CASE FOUND
 
 	fi
 
